6ec41138656931216063ba38d38933f6799f5476
PA106G2880325 <owinobrandon45@gmail.com>
2025-11-04 10:39:11 +0300

    Add files via upload

---
+++ CAT2_Q2.C
@@ -0,0 +1,39 @@
+/*
+Neame :Brandon Moses Owino
+Reg No: PA106/G/28803/25
+
+CAT 2 Question 2
+
+*/
+
+//roman one
+Array is an ordered collection of finite elements of the
+  same data type and stored at adjuscent memory location
+ 
+ //Roman two
+ 
+ int scores[2][2] = {
+   
+ 	
+    {65,92},
+	{84,72},
+		
+	{35,70},
+	{59,67}
+		
+   }	
+   
+//Roman three
+for(i = 0;i < 4;i++){
+	for(j = o;j < 4;j++){
+		printf("%d,scores);
+
+	
+		
+	
+		
+		
+	
+ 	
+    
+ 
